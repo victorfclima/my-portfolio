9fef520694a71155d616ca43149c44f491c4b8cc
Victor França <victorfclima@gmail.com>
2020-07-23 00:24:04 -0300

    Updates

---
+++ precache-manifest.57e3c898c9f79af766f04d7b488079c1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8407d44170d83195588e376a28ff3732",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "31aefd10a8126a2d0c69",
+    "url": "/my-portfolio/static/css/main.2766a119.chunk.css"
+  },
+  {
+    "revision": "d24f4fc5913a1a2fa262",
+    "url": "/my-portfolio/static/js/2.857af706.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.857af706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31aefd10a8126a2d0c69",
+    "url": "/my-portfolio/static/js/main.66db3ce4.chunk.js"
+  },
+  {
+    "revision": "6d7a7337562760299d40",
+    "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
+  },
+  {
+    "revision": "4ca8b2e2b98eaac66c830c1b962126c6",
+    "url": "/my-portfolio/static/media/ecoleta.4ca8b2e2.png"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/my-portfolio/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "14542070a350b85540ba1e1079e25319",
+    "url": "/my-portfolio/static/media/portfolio.14542070.png"
+  },
+  {
+    "revision": "9f5827f43da3141b5efac0ab9b1fb118",
+    "url": "/my-portfolio/static/media/projphoto1.9f5827f4.png"
+  },
+  {
+    "revision": "714f191af02c414fca0a89605fd95154",
+    "url": "/my-portfolio/static/media/shopapp.714f191a.png"
+  },
+  {
+    "revision": "c6942781b8dc1f442248fd569f7222c5",
+    "url": "/my-portfolio/static/media/solenergy.c6942781.png"
+  },
+  {
+    "revision": "6203daab72be4b996ab5644078033d67",
+    "url": "/my-portfolio/static/media/weatherapp.6203daab.png"
+  }
+]);
